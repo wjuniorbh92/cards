aabef903639d81872f76b14899e2b9aefaa4f4e9
wjuniorbh92 <68206230+wjuniorbh92@users.noreply.github.com>
2021-08-17 15:14:15 -0300

    add create hand

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,7 +6,6 @@ defmodule Cards do
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
-
   end
 
   def shuffle(deck) do
@@ -15,5 +14,27 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
